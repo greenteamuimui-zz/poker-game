edb196adabde1c1717ce109859b8eff9bc6ae46f
Cherry Lam <siuding0601@gmail.com>
2017-06-07 18:08:36 -0700

    Staeted hand spec and class to deal with logic of which is a better hand

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,1 +1,34 @@
 require 'rspec'
+require 'hand'
+
+describe Hand do
+  subject(:hand) {Hand.new}
+  let(:c1) {Card.new(2, "S")}
+  let(:c2) {Card.new(2, "S")}
+  let(:c3) {Card.new(3, "S")}
+  let(:c4) {Card.new(4, "S")}
+  let(:c5) {Card.new(5, "S")}
+
+  describe "#initialize" do
+    it "starts the hand with zero cards" do
+      expect(hand.hand).to eq([])
+    end
+  end
+
+  describe "#read" do
+    it "returns an array of the cards' value and suit in an array" do
+      hand.hand = []
+    end
+  end
+
+
+  describe "#one_pair" do
+    hand.hand = []
+    it "check whether the hand has only one pair" do
+      expect(hand.hand.one_pair).to eq true
+    end
+  end
+
+
+
+end
